0368999f67ef5c72faf1bbca861581d8458ff3dc
itabhijitb <itabhijitb@gmail.com>
2015-09-28 01:56:27 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,10 @@
+## The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+##     set the value of the matrix
+##     get the value of the matrix
+##     set the value of the inverse
+##     get the value of the inverse
+
+## Function to creates a cachable matrix
 makeCacheMatrix  <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -12,6 +19,20 @@ makeCacheMatrix  <- function(x = matrix()) {
              getinv = getinv)
 }
 
+## Solves the cachable matrix
+## Usage
+##      cacheSolve(makeCacheMatrix(a),b,...)
+
+##      ## Default S3 method:
+##      > B
+##           [,1] [,2]
+##      [1,]    1    3
+##      [2,]    2    4
+##      > cacheSolve(makeCacheMatrix(B))
+##           [,1] [,2]
+##      [1,]   -2  1.5
+##      [2,]    1 -0.5
+##      > 
 cacheSolve  <- function(x, ...) {
         m <- x$getinv()
         if(!is.null(m)) {
